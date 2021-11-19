83d7a02281f5ab696f7d6c824ec380410fbc4765
Alex-TV <kovalev.sasha82@gmail.com>
2021-11-19 20:21:32 +0300

    create project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+
+
+public class Listener
+{
+
+private HttpListener listener = new HttpListener();
+
+public async Task Listen()
+{
+   listener.Prefixes.Add("https://localhost:443/provider/");
+   
+}
+
+}
